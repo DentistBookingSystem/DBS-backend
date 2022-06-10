5f0f8f932a71f8b9075a72d5d5ec7d6dce0390af
ADMIN <hiddenwory@gmail.com>
2022-06-10 17:24:15 +0700

    Merge-code

---
+++ src/main/java/com/rade/dentistbookingsystem/controller/admin/AccountAdminController.java
@@ -0,0 +1,24 @@
+package com.rade.dentistbookingsystem.controller.admin;
+
+
+import com.rade.dentistbookingsystem.domain.Account;
+import com.rade.dentistbookingsystem.services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("rade/admin/account")
+public class AccountAdminController {
+
+    @Autowired
+    AccountService accountService;
+
+    @GetMapping("accountDetail")
+    public Account findByPhone(@RequestParam String phone) {
+        return accountService.findByPhone(phone);
+    }
+
+}
