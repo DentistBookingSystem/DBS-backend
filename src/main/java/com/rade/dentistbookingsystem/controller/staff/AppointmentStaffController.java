6d70adc951bb40d38b6c294075ee8543d1cc6c46
kien <masterkien1284@gmail.com>
2022-06-25 17:24:47 +0700

    fix cancel appointment by staff

--- src/main/java/com/rade/dentistbookingsystem/controller/staff/AppointmentStaffController.java
+++ src/main/java/com/rade/dentistbookingsystem/controller/staff/AppointmentStaffController.java
@@ -1,6 +1,7 @@
 package com.rade.dentistbookingsystem.controller.staff;
 
 import com.rade.dentistbookingsystem.componentform.AppointmentComponentForFilter;
+import com.rade.dentistbookingsystem.componentform.AppointmentIdListAndDescriptionToCancel;
 import com.rade.dentistbookingsystem.componentform.AppointmentWithDetails;
 import com.rade.dentistbookingsystem.componentform.JsonPhone;
 import com.rade.dentistbookingsystem.domain.Appointment;
@@ -15,6 +16,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -59,12 +61,16 @@ public class AppointmentStaffController {
         return null;
     }
 
-    @GetMapping("cancel")
+    @PostMapping("cancel")
     @Transactional(rollbackFor = Exception.class)
-    public ResponseEntity<?> cancelAppointment(@RequestParam int id, @RequestParam String description) {
-        Appointment appointment = appointmentService.cancelAppointmentForAdmin(id);
-        notificationService.createNotificationForCancellingAppointmentFromAdmin(appointment, description);
-        return ResponseEntity.ok(appointment);
+    public ResponseEntity<?> cancelAppointment(@RequestBody AppointmentIdListAndDescriptionToCancel appointmentIdListAndDescriptionToCancel) {
+        int[] appointmentIdList = appointmentIdListAndDescriptionToCancel.getAppointmentIdList();
+        String description = appointmentIdListAndDescriptionToCancel.getDescription();
+        for (int id : appointmentIdList) {
+            Appointment appointment = appointmentService.cancelAppointmentForAdmin(id);
+            notificationService.createNotificationForCancellingAppointmentFromAdmin(appointment, description);
+        }
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @PostMapping("markdone/{id}")
