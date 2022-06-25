cd2cc4c2083cc13efc457c8b981b335e9cb89161
kien <masterkien1284@gmail.com>
2022-06-25 17:50:03 +0700

    add get dr list by branch for staff

---
+++ src/main/java/com/rade/dentistbookingsystem/controller/staff/DoctorStaffController.java
@@ -0,0 +1,20 @@
+package com.rade.dentistbookingsystem.controller.staff;
+
+import com.rade.dentistbookingsystem.domain.Doctor;
+import com.rade.dentistbookingsystem.services.DoctorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("rade/staff/doctor")
+public class DoctorStaffController {
+    @Autowired
+    DoctorService doctorService;
+    @GetMapping("{branchId}")
+    public List<Doctor> getByBrandId(@PathVariable int branchId){
+        return doctorService.findByBranchId(branchId);
+    }
+}
