c049bcbdd8366f8cb76f6476ef389124e847708b
ADMIN <hiddenwory@gmail.com>
2022-06-26 21:54:52 +0700

    Edit account and Register account

--- src/main/java/com/rade/dentistbookingsystem/model/AccountDTO.java
+++ src/main/java/com/rade/dentistbookingsystem/model/AccountDTO.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.io.Serializable;
 
 @AllArgsConstructor
@@ -23,6 +24,8 @@ public class AccountDTO implements Serializable {
     private int districtId;
     @NotNull(message = "Phone is required")
     private String phone;
+    @Pattern( regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$")
     private String email;
     private int status;
 
