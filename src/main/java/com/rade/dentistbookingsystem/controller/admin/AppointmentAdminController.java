dabbbd3d8e7057f5b555d3f991b4a4160acc1778
kien <masterkien1284@gmail.com>
2022-07-12 07:06:46 +0700

    add view appointment detail for admin

--- src/main/java/com/rade/dentistbookingsystem/controller/admin/AppointmentAdminController.java
+++ src/main/java/com/rade/dentistbookingsystem/controller/admin/AppointmentAdminController.java
@@ -1,7 +1,10 @@
 package com.rade.dentistbookingsystem.controller.admin;
 
 import com.rade.dentistbookingsystem.componentform.AppointmentComponentForFilter;
+import com.rade.dentistbookingsystem.componentform.AppointmentWithDetails;
 import com.rade.dentistbookingsystem.domain.Appointment;
+import com.rade.dentistbookingsystem.domain.AppointmentDetail;
+import com.rade.dentistbookingsystem.services.AppointmentDetailService;
 import com.rade.dentistbookingsystem.services.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +18,25 @@ public class AppointmentAdminController {
     @Autowired
     AppointmentService appointmentService;
 
+    @Autowired
+    AppointmentDetailService appointmentDetailService;
+
     @PostMapping("filter")
     public List<Appointment> findAppointmentByMakingDateAndStatus(@RequestBody AppointmentComponentForFilter appointmentComponentForFilter) {
         try {
             return appointmentService.filterAppointment(appointmentComponentForFilter);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @PostMapping("find/{id}")
+    public AppointmentWithDetails findAppointmentByPhone(@PathVariable Integer id) {
+        try {
+            Appointment appointment = appointmentService.findId(id);
+            List<AppointmentDetail> appointmentDetailList = appointmentDetailService.findByAppointmentId(appointment.getId());
+            return new AppointmentWithDetails(appointment, appointmentDetailList);
         } catch (Exception e) {
             e.printStackTrace();
         }
