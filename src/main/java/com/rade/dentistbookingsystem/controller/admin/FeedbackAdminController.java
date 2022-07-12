b7ddfcb9972907da158a5a53c0394bdf5e44e5fd
ADMIN <hiddenwory@gmail.com>
2022-07-12 23:46:28 +0700

    Feedback admin

--- src/main/java/com/rade/dentistbookingsystem/controller/admin/FeedbackAdminController.java
+++ src/main/java/com/rade/dentistbookingsystem/controller/admin/FeedbackAdminController.java
@@ -1,11 +1,14 @@
 package com.rade.dentistbookingsystem.controller.admin;
 
+import com.rade.dentistbookingsystem.Constant;
 import com.rade.dentistbookingsystem.componentform.PageForFeedback;
 import com.rade.dentistbookingsystem.domain.Feedback;
 import com.rade.dentistbookingsystem.services.AccountService;
 import com.rade.dentistbookingsystem.services.FeedbackService;
 import com.rade.dentistbookingsystem.services.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,5 +35,26 @@ public class FeedbackAdminController {
                 pageForFeedback.getStatus(),
                 pageForFeedback.getServiceId(),
                 pageForFeedback.getTime());
+    }
+    @PostMapping("approve/{id}")
+    @Transactional(rollbackFor = Exception.class)
+    public ResponseEntity<?> approveFeedback(@PathVariable int id) {
+        Feedback feedback = feedbackService.updateFeedbackStatus(id, Constant.FEEDBACK_STATUS_APPROVE);
+        notificationService.createNotificationForApprovingFeedbackFromAdmin(feedback);
+        return ResponseEntity.ok().build();
+
+
+    }
+
+    @PostMapping("disapprove/{id}")
+    @Transactional(rollbackFor = Exception.class)
+    public ResponseEntity<?> disapproveFeedback(@PathVariable int id) {
+        Feedback feedback = feedbackService.updateFeedbackStatus(id, Constant.FEEDBACK_STATUS_DISAPPROVE);
+        notificationService.createNotificationForDisapprovingFeedbackFromAdmin(feedback);
+        int accountId = feedback.getAppointment().getAccount().getId();
+        if (feedbackService.checkAccountToBanByFeedback(accountId)) {
+            accountService.checkAccount(Constant.ACCOUNT_STATUS_INACTIVE, accountId);
+        }
+        return ResponseEntity.ok().build();
     }
 }
