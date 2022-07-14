9e42fc89e9fb92d1bac45d92ece01103636fdca6
kien <masterkien1284@gmail.com>
2022-07-15 02:12:27 +0700

    finish

--- src/main/java/com/rade/dentistbookingsystem/controller/patient/AccountPatientController.java
+++ src/main/java/com/rade/dentistbookingsystem/controller/patient/AccountPatientController.java
@@ -30,7 +30,7 @@ public class AccountPatientController {
     public ResponseEntity<?> edit(@Validated @RequestBody AccountDTO accountDTO) {
         try {
 
-            if (accountService.confirmPassword(accountDTO.getPhone(), accountDTO.getPassword())) {
+            if (accountService.confirmPassword(accountDTO.getPhone(), accountDTO.getConfirmPassword())) {
                 return ResponseEntity.ok(accountService.edit(accountDTO));
             } else {
                 return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body("Your old password is wrong");
